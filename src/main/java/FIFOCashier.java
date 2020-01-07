00fc1459050eaf4678cb906fe0c80997aa83e85a
Ryan <ryan.j.z@hotmail.com>
2020-01-07 22:12:52 +0100

    Update FIFOCashier.java
    do work until

--- src/main/java/FIFOCashier.java
+++ src/main/java/FIFOCashier.java
@@ -31,17 +31,17 @@ public class FIFOCashier extends Cashier {
             ) {
                 amount += expectedCheckOutTime(c.getNumberOfItems());
             }
-            if(this.servingCustomer != null){
+            if (this.servingCustomer != null) {
                 amount += this.expectedCheckOutTime(this.servingCustomer.getNumberOfItems()) - this.getCurrentTime().getSecond();
             }
         }
         //This condition goes if both the waiting queue is 0 and the cashier is serving a customer.
-        if(this.waitingQueue.size() == 0 && this.servingCustomer != null){
+        if (this.waitingQueue.size() == 0 && this.servingCustomer != null) {
             amount = this.expectedCheckOutTime(this.servingCustomer.getNumberOfItems()) - this.getCurrentTime().getSecond();
         }
 
         //Werkt
-        if(this.waitingQueue.size() == 0 && this.servingCustomer == null){
+        if (this.waitingQueue.size() == 0 && this.servingCustomer == null) {
             amount = 0;
         }
         return amount;
@@ -49,21 +49,42 @@ public class FIFOCashier extends Cashier {
 
     @Override
     public void doTheWorkUntil(LocalTime targetTime) {
+        //all cashiers proceed their work until targetTime
+        // continue or finish checkout of the current customer if there is any customer, but no longer than until the targetTime
+        // if there is time left go next
+        //if the queue is empty, take a break
+        //else go to the next customer from the queue and go through this process again
+        //else it means time = T (???)
 
-        if(this.servingCustomer == null && this.waitingQueue.size() == 0){
-            this.setTotalIdleTime(this.getTotalIdleTime() + (int) ChronoUnit.SECONDS.between(this.getCurrentTime(), targetTime));
-        }
-
-        if(this.waitingQueue.size() != 0 && this.servingCustomer == null){
-            this.servingCustomer = this.waitingQueue.peek();
-            this.setCurrentTime(this.getCurrentTime().plusSeconds(this.expectedCheckOutTime(this.servingCustomer.getNumberOfItems())));
-            this.waitingQueue.remove(this.waitingQueue.peek());
-            this.servingCustomer = null;
-
-            if(this.waitingQueue.size() == 0){
-                this.setTotalIdleTime(this.getTotalIdleTime() + (int) ChronoUnit.SECONDS.between(this.getCurrentTime(), targetTime));
+        while (this.currentTime.isBefore(targetTime)) {
+            if (this.waitingQueue.isEmpty()) {
+                if (this.servingCustomer == null) {
+                    this.setTotalIdleTime(this.getTotalIdleTime() + (int) ChronoUnit.SECONDS.between(this.getCurrentTime(), targetTime));
+                }
+            } else {
+                this.servingCustomer = waitingQueue.peek();
+                this.waitingQueue.remove(this.waitingQueue.peek());
+                this.setCurrentTime(this.getCurrentTime().plusSeconds(this.expectedCheckOutTime(this.servingCustomer.getNumberOfItems())));
+                continue;
             }
+            this.setCurrentTime(targetTime);
         }
-        this.setCurrentTime(targetTime);
+
+
+//        if(this.servingCustomer == null && this.waitingQueue.size() == 0){
+//
+//        }
+//
+//        if(this.waitingQueue.size() != 0 && this.servingCustomer == null){
+//            this.servingCustomer = this.waitingQueue.peek();
+//            this.setCurrentTime(this.getCurrentTime().plusSeconds(this.expectedCheckOutTime(this.servingCustomer.getNumberOfItems())));
+//            this.waitingQueue.remove(this.waitingQueue.peek());
+//            this.servingCustomer = null;
+//
+//            if(this.waitingQueue.size() == 0){
+//                this.setTotalIdleTime(this.getTotalIdleTime() + (int) ChronoUnit.SECONDS.between(this.getCurrentTime(), targetTime));
+//            }
+//        }
+//        this.setCurrentTime(targetTime);
     }
 }
